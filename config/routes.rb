47a24b632f760689ee1b6179efd8a5b8eb59a2ad
Frédéric Bui <bui.frederic.pro@gmail.com>
2022-12-28 15:27:11 +0100

    ajax routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  post '/ajax/trigger', to: "ajax#trigger"
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "ajax#index"
 end
